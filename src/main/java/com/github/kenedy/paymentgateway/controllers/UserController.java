25cfa3b2f8176fb3c91c48034f5d580c73486060
kenedy <kenedymirandadasilva@gmail.com>
2026-09-10 23:25:39 -0300

    feat: add GET /users and GET /users/{id} endpoints

--- src/main/java/com/github/kenedy/paymentgateway/controllers/UserController.java
+++ src/main/java/com/github/kenedy/paymentgateway/controllers/UserController.java
@@ -1,5 +1,12 @@
 package com.github.kenedy.paymentgateway.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +37,20 @@ public class UserController {
         repository.save(user);
         return user;
     } 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable UUID id) {
+        Optional<User> user = repository.findById(id);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return repository.findAll();
+    }
 }
